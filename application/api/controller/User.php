46b1514c8400385da15f861c4bbc62414c898357
JiPai <github@jipai.moe>
2019-02-18 00:00:23 +0800

    除Index外的路径访问统一返回准许跨域头。

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -1,8 +1,19 @@
 <?php
 namespace app\api\controller;
 
-class User
+use think\Controller;
+use think\facade\Session;
+
+class User extends Controller
 {
+    protected $beforeActionList=[
+        'disableCorb'=>['except'=>'index']
+    ];
+    public function disableCorb(){
+        header("Access-Control-Allow-Origin:*");
+        header("Access-Control-Allow-Methods:GET,POST,PUT,DELETE,OPTIONS");
+        header("Access-Control-Allow-Headers:Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    }
     public function index()
     {
         return '<section><h1>newGame,故事的起点</h1><p>您访问的是newGame API接口，文档访问云雀平台，感谢使用。beta1.0</p></section> <style>section{position:absolute;bottom:50px;right:70px;color:#dedede;text-align:right;}body{background-color:#171717;}</style>';
