a288087ca7dfe091266d1bd6c8d477c5d08e9da8
JiPai <github@jipai.moe>
2019-02-18 00:02:22 +0800

    完善了用户模型中的注册逻辑

--- application/api/model/User.php
+++ application/api/model/User.php
@@ -45,34 +45,49 @@ class User extends Model
 
     public function register(){
         $email = Request::post('email');
-        $passWord = Request::post('password');
+        //$passWord = Request::post('password');
+        $passWord = password_hash(Request::post('password'),PASSWORD_DEFAULT);
+        $phone=Request::post('phone');
+        $gender=Request::post('gender');
+        $realName=Request::post('realname');
+        $nickname=Request::post('nickname');
+
         $validate = new \app\api\validate\User();
-        if (!$validate->scene('email')->check(['email' => $email])) {
-            //验证登录输入是否为邮箱
-            return $loginResult = [
-                'loginStatus' => '0',
+        if (!$validate->scene('registerCheck')->check(['email' => $email,'password'=>$passWord,'nickname'=>$nickname,'realname'=>$realName,'phone'=>$phone,'gender'=>$gender])) {
+            //验证所有传入是否符合规则
+            return $registerResult = [
+                'registerStatus' => '0',
                 'errMsg' => $validate->getError()
             ];
         } else {
-            //邮箱格式正确，进入登录
+            //查看是否已注册
             $user = User::where('email', $email)->find();
             if ($user) {
-                if (password_verify($passWord, $user->password)) {
-                    return $loginResult = [
-                        'loginStatus' => '1',
+                    return $registerResult= [
+                        'registerStatus' => '0',
+                        'errMsg' => '咦？这个邮箱已经在记录里啦，请尝试登录或者找回密码'
+                    ];
+            } else {
+                $user=new User;
+                $dbinsertResult=$user->save([
+                    'username'=>$realName,
+                    'nickname'=>$nickname,
+                    'password'=>$passWord,
+                    'email'=>$email,
+                    'gender'=>$gender,
+                    'phone'=>$phone
+                ]);
+                if($dbinsertResult==1){
+                    return $registerResult = [
+                        'registerStatus' => '1',
                         'errMsg' => ''
                     ];
-                } else {
-                    return $loginResult = [
-                        'loginStatus' => '0',
-                        'errMsg' => 'Password dont match record.'
+                }else{
+                    return $registerResult = [
+                        'registerStatus' => '0',
+                        'errMsg' => 'Something wrong happen.'
                     ];
                 }
-            } else {
-                return $loginResult = [
-                    'loginStatus' => '0',
-                    'errMsg' => 'Email dont match record.'
-                ];
             };
         }
     }
