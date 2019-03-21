071d1dc6f8cd2f764a9a5ca03c46d81adb03555c
JiPai <github@jipai.moe>
2019-03-21 21:59:14 +0800

    新增Club对应的模型

---
+++ application/api/model/Club.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\api\model;
+
+use think\Model;
+use think\facade\Request;
+use think\facade\Session;
+
+class Club extends Model
+{
+    public function addClub()
+    {
+        $clubName=Request::post('clubName');
+        $imgUrl=Request::post('imgUrl');
+        $intro=Request::post('intro');
+        $clubBelong=Request::post('clubBelong');
+        $clubSort=Request::post('clubSort');
+        $clubManager=Request::post('clubAdmin');
+        $joinMode=Request::post('joinMode');
+        $status=Request::post('status');
+        //这里应该对所有变量做判空检查
+        $club = Club::where('name', $clubName)->find();
+        if($club){
+            return $addClubResult=[
+                'addClubResult'=>'0',
+                'errMsg'=>'已经有重名社团存在了！'
+            ];
+        }
+        else{
+            $club =new Club;
+            $dbInsertResult = $club->save([
+                'name' => $clubName,
+                'intro' => $intro,
+                //'manager_user' => $clubManager,
+                'status' => $status,
+                'join_mode' => $joinMode,
+                'sort' => $clubSort,
+                'belong'=>$clubBelong,
+                'img_logo'=>$imgUrl
+            ]);
+            if ($dbInsertResult == 1) {
+                return $addClubResult = [
+                    'addClubResult' => '1',
+                    'errMsg' => ''
+                ];
+            } else {
+                return $addClubResult = [
+                    'addClubResult' => '0',
+                    'errMsg' => '添加失败，联系管理员'
+                ];
+            }
+        }
+    }
+}
